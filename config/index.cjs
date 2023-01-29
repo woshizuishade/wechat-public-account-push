8248ee4ac895118f80d465343bf5ec0d53dc1fcf
woshizuishade <32673325+woshizuishade@users.noreply.github.com>
2023-01-29 14:57:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,48 +6,51 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  USE_PASSAGE: 'server-chan',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx798451ed236a1988',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '57e46b88b9f6d3568cabe1c4eb8acb7e',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '天津',
+  CITY: '天津',
 
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    "USERS":[
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
+            "name":"苏大美女",
+            "id":"omNiO59NUN1ApifPFTf291VZxw3c",
+            "useTemplateId":"0001",
+            "province":"天津",
+            "city":"天津",
+            "horoscopeDate":"02-28",
+            "horoscopeDateType":"今日",
+            "openUrl":"https://shuangxunian.github.io/",
+            "festivals":[
+                {
+                    "type":"*生日",
+                    "name":"生日",
+                    "date":"02-28",
+                    "year":"1994"
+                }
+            ],
+            "customizedDateList":[
+                {
+                    "keyword":"成为同志",
+                    "date":"2018-05-04"
+                },
+                {
+                    "keyword":"成为战友",
+                    "date":"2022-05-05"
+                },
+                {
+                    "keyword":"曲奇",
+                    "date":"2022-11-08"
+                }
+            ]
+        }
+    ],
+
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
@@ -60,7 +63,18 @@ const USER_CONFIG = {
       id: '',
     }
   ],
-
+    "SWITCH":{
+        "weather":true,
+        "holidaytts":true,
+        "CIBA":true,
+        "oneTalk":true,
+        "earthyLoveWords":true,
+        "momentCopyrighting":false,
+        "poisonChickenSoup":false,
+        "poetry":true,
+        "horoscope":false,
+        "birthdayMessage":true
+    }
 }
 
 module.exports = USER_CONFIG
